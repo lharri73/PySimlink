5a5a3266c354421e5ccb5b89df22942ad0e1ce1a
Landon Harris <lharri73@vols.utk.edu>
2023-02-03 11:26:56 -0500

    removed <variant>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pysimlink",
-    version="1.2.0",
+    version="1.2.1-dev0",
     author="Landon Harris",
     author_email="lharri73@vols.utk.edu",
     description="Compile, run, and interact with Simulink models natively in Python!",
