89d185aa5c8483c6db1907780bbddc1408c1ea7b
Landon Harris <lharri73@vols.utk.edu>
2023-02-08 09:51:35 -0500

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pysimlink",
-    version="1.2.1-dev2",
+    version="1.2.1-dev3",
     author="Landon Harris",
     author_email="lharri73@vols.utk.edu",
     description="Compile, run, and interact with Simulink models natively in Python!",
