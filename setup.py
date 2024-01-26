3c15b2a0f77aabdb3f3dffa99d878c73ee1c9881
Landon Harris <lharri73@vols.utk.edu>
2024-01-26 13:08:26 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("requirements.txt", "r") as f:
     reqs = f.read().splitlines()
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pysimlink",
-    version="1.2.1-dev3",
+    version="1.2.1",
     author="Landon Harris",
     author_email="lharri73@vols.utk.edu",
     description="Compile, run, and interact with Simulink models natively in Python!",
@@ -38,8 +38,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
     ],
     url="https://github.com/lharri73/PySimlink",
     project_urls={
